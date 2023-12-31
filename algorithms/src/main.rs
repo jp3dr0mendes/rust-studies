7d1704ca67145b7203a6b4467f5041d9ddd3b2c3
Pedro <joao.pedro73210@gmail.com>
2023-12-30 21:49:31 -0300

    binary search implement - OK

--- algorithms/src/main.rs
+++ algorithms/src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::io;
 
 //buble sort - OK
 //insertion sort
@@ -23,25 +24,64 @@ fn buble_sort(mut array: [usize;10]) -> [usize;10] {
                 array[j+1] = aux[0];
                 array[j] = aux[1];
             }
-
-            println!("array on iteration{i}: {:?}", array);
         }
     }
 
     return array;
 }
 
+fn binary_search(array: [usize;10], number: usize) -> (bool, usize){
+
+    let mut on_array: bool = false;
+    let mut pivo: usize = (array.len()-1)/2;
+
+    if array[pivo] == number{
+        on_array = true;
+        return (on_array, pivo);
+    } else {
+        while pivo != 0 && pivo != array.len()-1 {
+
+            if number > array[pivo]{
+                pivo = ((array.len()) + pivo) / 2; //para garantir que chegue ao final do array
+            } else {
+                pivo = pivo / 2; 
+            }
+
+            if array[pivo] == number {
+                on_array = true;
+                return (on_array, pivo);                
+            }
+        }
+        return (on_array, pivo);
+    }
+}
 
 fn main() {
-    let list_size: usize = 10;
-    let mut array: [usize;10] = [0;10];
+        let list_size: usize = 10;
+        let mut array:  [usize;10] = [0;10];
+        let mut num: String = String::new();
+    
+        for i in 0..list_size {
+            array[i] = rand::thread_rng().gen_range(0..=100);
+        }
+    
+        println!("O array resultante eh {:?}", array);
+    
+        let order_array: [usize;10] = buble_sort(array);
 
-    for i in 0..list_size {
-        array[i] = rand::thread_rng().gen_range(0..=100);
+        println!("Order array: {:?}", order_array);
+        println!("Type a number you'd like to search on array:");
+
+        io::stdin().read_line(&mut num).expect("Error on reading element!");
+
+        let num:usize = num.trim().parse().expect("Please type a number!");
+        let result: (bool, usize) = binary_search(order_array, num);
+
+        println!("result: {}", result.0);
+
+        if result.0 {
+            println!("The number {num} is on index {}", result.1);
+        } else {
+            println!("Error on find the number on array!");
+        }
     }
-
-    println!("O array resultante eh {:?}", array);
-
-    let order_array: [usize;10] = buble_sort(array);
-    println!("Order array: {:?}", order_array);
-}
