d2c5263ce9299b6a9d61cdd5355695a6491c8e0f
joao pedro <joao.albuquerque@irede.org.br>
2024-01-23 16:23:27 -0300

    refactoring

--- linear_regression/src/main.rs
+++ linear_regression/src/main.rs
@@ -6,5 +6,8 @@ mod models{
 use crate::models::linear_regression::linear_regression::LinearRegression;
 fn main() {
     // let mut test: Dataset = Dataset::new(r"C:\Users\joaop\Documents\projects\rust-studies\linear_regression\src\models\dataset.txt");
+    let mut Model: LinearRegression = LinearRegression::new(r"C:\Users\joaop\Documents\projects\rust-studies\linear_regression\src\dataset.txt",
+                                                            0.0, 0.0);
 
+    Model.train_model(100, 0.5);
 }
